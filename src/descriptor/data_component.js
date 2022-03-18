903b93d7915c32c9b4973a66f9e21daf0919a16f
otya <otya128@users.noreply.github.com>
2022-03-18 21:12:38 +0900

    Fix additional_data_component_info

--- src/descriptor/data_component.js
+++ src/descriptor/data_component.js
@@ -16,8 +16,8 @@ class TsDescriptorDataComponent {
         objDescriptor.descriptor_tag = reader.uimsbf(8);
         objDescriptor.descriptor_length = reader.uimsbf(8);
 
-        objDescriptor.data_component_id = reader.uimsbf(8);
-        objDescriptor.additional_data_component_info = reader.readBytes(2 + objDescriptor.descriptor_length - (reader.position >> 3));
+        objDescriptor.data_component_id = reader.uimsbf(16);
+        objDescriptor.additional_data_component_info = reader.readBytes(objDescriptor.descriptor_length - 2);
 
         return objDescriptor;
     }
