e2287d34596e08e8f9eef8da3d26cace1050aadb
otya <otya281@gmail.com>
2026-08-12 14:16:51 +0900

    Use TextDecoder instead of iconv-lite to reduce bundle size

--- src/char.ts
+++ src/char.ts
@@ -1,5 +1,6 @@
 import { Buffer } from "buffer";
 import charTable = require("./char_table");
+const shiftJisDecoder = new TextDecoder("shift_jis");
 
 const charCode = {
     hiragana: 0x30,
@@ -67,7 +68,7 @@ class TsChar {
         }
 
         if (this.sjis.length > 0) {
-            this.result += decode(Buffer.from(this.sjis), "shift-jis");
+            this.result += shiftJisDecoder.decode(Buffer.from(this.sjis));
             this.sjis = [];
         }
 
@@ -263,7 +264,7 @@ class TsChar {
                         const second = this.getNext();
                         if (this.useUnicode(first, second)) {
                             if (this.sjis.length > 0) {
-                                this.result += decode(Buffer.from(this.sjis), "shift-jis");
+                                this.result += shiftJisDecoder.decode(Buffer.from(this.sjis));
                                 this.sjis = [];
                             }
                             this.result += this.getUnicode(first, second);
@@ -315,7 +316,7 @@ class TsChar {
                         const second = this.getNext() & 0x7F;
                         if (this.useUnicode(first, second)) {
                             if (this.sjis.length > 0) {
-                                this.result += decode(Buffer.from(this.sjis), "shift-jis");
+                                this.result += shiftJisDecoder.decode(Buffer.from(this.sjis));
                                 this.sjis = [];
                             }
                             this.result += this.getUnicode(first, second);
